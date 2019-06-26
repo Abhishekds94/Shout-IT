b547199a72ae290b04aac7855059283534b277a6
Abhishek D S <abhishekds94@gmail.com>
2019-06-26 21:00:02 +0530

    Final push

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'database.php'; ?>
 <?php
 	//Create Select Query
-	$query ="SELECT * FROM shouts";
+	$query = "SELECT * FROM shouts ORDER BY id DESC";
 	$shouts = mysqli_query($con, $query);
 ?>
 <!DOCTYPE html>
@@ -18,19 +18,20 @@
 			</header>
 			<div id="shouts">
 				<ul>
-				<?php while($row = mysqli_fetch_assoc($shouts)) : ?>
-					<li class="shout"><span>
-						<?php echo $row['time'] ?> - </span><b><?php echo $row['user'] ?>:</b> <?php echo $row['message'] ?>  </li>
-				<?php endwhile; ?>
-					
+					<?php while($row = mysqli_fetch_assoc($shouts)) : ?>
+						<li class="shout"><span><?php echo $row['time'] ?> - </span><strong><?php echo $row['user'] ?>:</strong> <?php echo $row['message'] ?> </li>
+					<?php endwhile; ?>				
 				</ul>
 			</div>
 			<div id="input">
+				<?php if(isset($_GET['error'])) : ?>
+					<div class="error"><?php echo $_GET['error']; ?></div>
+				<?php endif; ?>
 				<form method="post" action="process.php">
 					<input type="text" name="user" placeholder="Enter Your Name" />
-					<input type="text" name="message" placeholder="Enter Your Message" />
+					<input type="text" name="message" placeholder="Enter A Message" />
 					<br />
-					<input class="shout-btn" type="submit" name="submit" value="Shout It!" />
+					<input class="shout-btn" type="submit" name="submit" value="Shout It Out" />
 				</form>
 			</div>
 		</div>
